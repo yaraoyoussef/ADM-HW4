ced62c9b55376e78d4912c8cf2767ab47cc0f2b5
yaraoyoussef <118832851+yaraoyoussef@users.noreply.github.com>
2024-11-21 17:13:11 +0100

    finalization until hashing.py functions
    everything after that is still under construction :)

--- MyFunctions/hashing.py
+++ MyFunctions/hashing.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np 
 ''' 
 Defining multiple hash functions to experiment 
 '''
@@ -15,31 +16,33 @@ def polynomial_hash(x, a, b, p):
 def multiplicative_hash(x, a, b, p):
     return (a * x) % p
 
-'''  
+'''
 This function will hash each user's watched movie list, creating a representation 
 that allows for quick comparisons of user similarities.
 the variable 'num_hashes' allows us to experiment with different numbers of hash functions 
 '''
+
 def minhash(user_movies, num_hashes, selected_hash):
     signatures = {}
     # Prime number for hash modulus. Large to prevent hash collisions 
     p = 9999991  
+    coefficients = [(random.randint(1, 100), random.randint(0, 100)) for _ in range(num_hashes)]
+    hash_functions = [lambda x, a=a, b=b, p=p: selected_hash(x, a, b, p) for a, b in coefficients]
 
     # For each user
-    for user in user_movies:
+    for user, movies in user_movies.items():
+        movies = np.array(list(movies))
          # Initialize a signature vector with infinity values
-        signature = [float('inf')] * num_hashes
+        signature = np.full(num_hashes, float('inf'))
          # For each movie rated by the user
-        for movie in user_movies[user]:
-            # For each hash function
-            for i in range(num_hashes):
-                a = random.randint(1, 100)
-                b = random.randint(1, 100)
-                # Hash the movie ID using
-                hash_value = selected_hash(movie, a, b, p)
-                 # Update the signature with the smallest hash value
-                signature[i] = min(signature[i], hash_value)
-        # Store the MinHash signature for the current user in the dictionary
-        signatures[user] = signature
+        for i, hash_fn in enumerate(hash_functions):
+            # Apply the hash function to all movies
+            hash_values = hash_fn(movies)
+            
+            # Update the signature with the smallest hash value
+            signature[i] = np.min(hash_values)
+        
+        # Store the user's signature
+        signatures[user] = signature.tolist()
     
-    return signatures
+    return signatures 
